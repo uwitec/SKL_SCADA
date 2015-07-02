e093d7cab1531992e4943675a407cfb0be5ac4f5
咩安 <531779485@qq.com>
2015-07-02 15:32:09 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	SKL_SCADA/BLL/bin/Debug/BLL.dll
    	SKL_SCADA/BLL/bin/Debug/BLL.pdb
    	SKL_SCADA/BLL/bin/Debug/DAL.dll
    	SKL_SCADA/BLL/bin/Debug/DAL.pdb
    	SKL_SCADA/BLL/obj/Debug/BLL.csprojResolveAssemblyReference.cache
    	SKL_SCADA/BLL/obj/Debug/BLL.dll
    	SKL_SCADA/BLL/obj/Debug/BLL.pdb
    	SKL_SCADA/DAL/bin/Debug/DAL.dll
    	SKL_SCADA/DAL/bin/Debug/DAL.pdb
    	SKL_SCADA/DAL/obj/Debug/DAL.csprojResolveAssemblyReference.cache
    	SKL_SCADA/DAL/obj/Debug/DAL.dll
    	SKL_SCADA/DAL/obj/Debug/DAL.pdb
    	SKL_SCADA/SKL_SCADA.suo
    	SKL_SCADA/SKL_SCADA/bin/BLL.dll
    	SKL_SCADA/SKL_SCADA/bin/BLL.pdb
    	SKL_SCADA/SKL_SCADA/bin/DAL.dll
    	SKL_SCADA/SKL_SCADA/bin/DAL.pdb
    	SKL_SCADA/SKL_SCADA/bin/SKL_SCADA.dll
    	SKL_SCADA/SKL_SCADA/bin/SKL_SCADA.pdb
    
    	SKL_SCADA/SKL_SCADA/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    
    	SKL_SCADA/SKL_SCADA/obj/Debug/SKL_SCADA.csprojResolveAssemblyReference.cache
    	SKL_SCADA/SKL_SCADA/obj/Debug/SKL_SCADA.dll
    	SKL_SCADA/SKL_SCADA/obj/Debug/SKL_SCADA.pdb

---
+++ SKL_SCADA/SKL_SCADA/MachinePage.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SKL_SCADA
+{
+    public partial class MachinePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
